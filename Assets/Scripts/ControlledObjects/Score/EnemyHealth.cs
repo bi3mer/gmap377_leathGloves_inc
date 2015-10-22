e603fcae76d7a9aea33780b40fa01cc82ad5c629
rowlandsc <cr549@drexel.edu>
2015-10-22 01:37:40 -0400

    Week5Changes
    -Added Damage to lasers
    -Adding a scoring system
    -Added a script for health for enemies
    -Misc stuff

---
+++ Assets/Scripts/ControlledObjects/Score/EnemyHealth.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public int startingHealth = 100;          // The amount of health the enemy starts the game with.
+    public int currentHealth;                    // The current health the enemy has.
+   
+    public int scoreValue = 10;               // The amount added to the player's score when the enemy dies.
+   
+
+   
+    Collider eCollider;                         // Reference to the collider.
+    bool isDead;                                  // Whether the enemy is dead.
+   
+
+    void Awake()
+    {
+       
+        eCollider = GetComponent<Collider>();
+
+        // Setting the current health when the enemy first spawns.
+        currentHealth = startingHealth;
+    }
+
+    void Update()
+    {
+        // If the enemy 
+        if (isDead)
+        {
+            // Increase the score by the enemy's score value.
+            ScoreManager.score += scoreValue;
+            Destroy(gameObject);
+        }
+
+    }
+
+    public void TakeDamage(int amount)
+    {
+        // If the enemy is dead...
+        if (isDead)
+            // ... no need to take damage so exit the function.
+            return;
+
+       
+
+        // Reduce the current health by the amount of damage sustained.
+        currentHealth -= amount;
+
+       
+
+        // If the current health is less than or equal to zero...
+        if (currentHealth <= 0)
+        {
+            // ... the enemy is dead.
+            Death();
+        }
+    }
+
+    void Death()
+    {
+        // The enemy is dead.
+        isDead = true;
+
+        
+
+    }
+}
---
+++ Assets/Scripts/ControlledObjects/Score/ScoreUpdaterForBullet.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreUpdaterForBullet : MonoBehaviour
+{
+    public int damagePerShot;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        Debug.Log("Fuck Alex");
+        if (col.gameObject.tag == "Enemy")
+        {
+            // Try and find an EnemyHealth script on the gameobject hit.
+            EnemyHealth enemyHealth = col.gameObject.GetComponent<EnemyHealth>();
+
+            // If the EnemyHealth component exist...
+            if (enemyHealth != null)
+            {
+                // ... the enemy should take damage.
+                enemyHealth.TakeDamage(damagePerShot);
+            }
+
+        }
+
+    }
+}
