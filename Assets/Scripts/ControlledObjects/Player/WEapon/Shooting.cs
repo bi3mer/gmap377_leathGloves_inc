91e829daa1b3ebcaa37dbd52ddf0e456b4a02dee
bfawber <brf35@drexel.edu>
2015-10-26 20:30:33 -0400

    Updated shooting.cs so the plane is no longer needed

--- Assets/Scripts/ControlledObjects/Player/WEapon/Shooting.cs
+++ Assets/Scripts/ControlledObjects/Player/WEapon/Shooting.cs
@@ -3,33 +3,34 @@ using System.Collections;
 
 public class Shooting : MonoBehaviour {
     
-	// Use this for initialization
-	void Start () {
-        
-
-    }
     public Transform character;
 
     public GameObject bullet; 
     public GameObject spwnPt;
 
+    /// <summary>
+    /// Called Every Frame
+    /// </summary>
     void Update()
     {
+        // only do anything when the button is pressed
         if (Input.GetMouseButtonDown(0))
-        { // only do anything when the button is pressed:
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-            if (Physics.Raycast(ray, out hit))
-            {
-                Debug.DrawLine(character.position, hit.point);
+        { 
+            // Bullet being created      
+            GameObject newBullet= Instantiate(bullet, spwnPt.transform.position, transform.rotation) as GameObject;
 
-                GameObject newBullet= Instantiate(bullet, spwnPt.transform.position, transform.rotation) as GameObject;
-                newBullet.GetComponent<Rigidbody>().velocity = ((hit.point - transform.position).normalized * GetComponent<Weapon>().Speed)  ;
-
-                //GameObject projectiles = Instantiate(bullet, spwnPt.transform.position, character.transform.rotation) as GameObject;
-                //projectiles.GetComponent<Rigidbody>().velocity = (hit.point - transform.position).normalized * 10;
-            }
+            // Make its velocity go towards the mouse
+            // NOTE: The vector being created uses the z field as the depth from the camera
+            // to the mouse point. I just made this 10000f so it knows where to go more
+            // accurately. It seems fine to me, but if aim fine tuning is needed check here.
+            // - Bryan
+            newBullet.GetComponent<Rigidbody>().velocity = (Camera.main.ScreenToWorldPoint(
+                new Vector3(
+                    Input.mousePosition.x,
+                    Input.mousePosition.y,
+                    10000f
+                    )
+                ).normalized * GetComponent<Weapon>().Speed) ;
         }
     }
-    
 }
