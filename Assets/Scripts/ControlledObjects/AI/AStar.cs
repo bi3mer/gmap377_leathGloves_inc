a64f06e0f70376cbf144f117af45cb83fb4700ff
bi3mer <colan@comcast.net>
2015-10-27 23:50:22 -0400

    AI Framework
    The stuff to do it is in there now, now i just have to work out the
    details and fix the vertex neighbor finder. If i can get that right then
    we'll bee in good shape :dancer:

---
+++ Assets/Scripts/ControlledObjects/AI/AStar.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AStar : MonoBehaviour {
+
+	// Use this for initialization
+	void Update () 
+    {
+        print(VertexNavigation.Instance.getMoves(0).Count);
+        print(VertexNavigation.Instance.getMoves(1).Count);
+        print(VertexNavigation.Instance.getMoves(2).Count);
+	}
+}
--- Assets/Scripts/Environment/Navigation/VertexNavigation.cs
+++ Assets/Scripts/Environment/Navigation/VertexNavigation.cs
@@ -7,7 +7,6 @@ public class VertexNavigation : MonoBehaviour
 	/// 	Create Singleton
 	/// </summary>
 	public static VertexNavigation instance;
-	
 	public static VertexNavigation Instance
 	{
 		get
@@ -20,24 +19,81 @@ public class VertexNavigation : MonoBehaviour
 		}
 	}
 
-	private Vector3[][] mappedSphere;
+    // TODO: comment
+    private Mesh mesh;
+
+    // TODO: comment
+    private Vector3[] vertices;
+
+    // TODO: comment
+    private float _radius;
+    public float radius 
+    {
+        get 
+        {
+            return this._radius;
+        }
+        set
+        { 
+            // pass
+        }
+    }
 	
 	/// <summary>
 	/// 	Map 3d sphere to 2d coordiante system
 	/// </summary>
 	void Start () 
 	{
+        // Get Radius
+        this._radius = GetComponent<SphereCollider>().radius * transform.localScale.x;
+
 		// Get mesh of planet
-		Mesh mesh = GetComponent<MeshFilter>().mesh;
+		this.mesh = GetComponent<MeshFilter>().mesh;
+        print(this.mesh);
 
-		// Get vertices of planet
-		Vector3[] vertices = mesh.vertices;
+        // Get copy of vertices
+        this.vertices = this.mesh.vertices;
 
-		// Convert
+        // Convert vertices to global coordiantes
+        float scale = transform.localScale.x;
+        for (int i = 0; i < this.mesh.vertexCount; ++i)
+        {
+            this.vertices[i] = this.vertices[i] * scale;
+        }
 	}
 
-	public Vector3[][] getMatrix()
-	{
-		return this.mappedSphere;
-	}
+    // TODO: make this array instead of list for optimization (term 2)
+    public ArrayList getMoves(int vertexIndex)
+    {
+        ArrayList verts = new ArrayList();
+        for (int i = 0; i < this.mesh.triangles.Length / 3; ++i)
+        {
+            bool found = false;
+
+            // Look for triangle that matches index
+            for (int j = 0; j < 3; ++j)
+            {
+                if (this.mesh.triangles[(i * 3) + j] == vertexIndex)
+                {
+                    found = true;
+                    break;
+                }
+            }
+
+            if (found)
+            {
+                for (int j = 0; j < 3; ++j)
+                {
+                    // TODO: rename tri
+                    int tri = verts.IndexOf((i * 3) + j);
+                    if (tri == -1 && tri != vertexIndex)
+                    {
+                        verts.Add(tri);
+                    }
+                }
+            }
+        }
+
+        return verts;
+    }
 }
