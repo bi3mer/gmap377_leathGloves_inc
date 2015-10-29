484fe7f57a5df5eff4417190160d50baa2960830
rowlandsc <cr549@drexel.edu>
2015-10-28 22:31:20 -0400

    Quick Adds
    -Simple timer script
    -added font

---
+++ Assets/Scripts/ControlledObjects/Score/SimpleTimer.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SimpleTimer : MonoBehaviour {
+
+    public float TimeS = 60.0f;
+    Text text;                      // Reference to the Text component.
+
+
+    void Awake()
+    {
+        // Set up the reference.
+        text = GetComponent<Text>();
+
+        // Reset the score.
+        TimeS = 60;
+    }
+
+    void Update()
+    {
+
+        TimeS -= Time.deltaTime;
+        text.text = "Time: " + (int)TimeS;
+        Debug.Log(TimeS);
+
+        if (TimeS <= 0)
+        {
+            timerEnded();
+        }
+
+    }
+
+    void timerEnded()
+    {
+       
+    }
+
+
+}
+
