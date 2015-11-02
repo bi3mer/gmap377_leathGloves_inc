2aa5f6f2bc6bb115d7d8315b50866de9c8ba410d
paperclip007 <alexbafaloukos@gmail.com>
2015-11-02 12:37:49 -0500

    First implementation of AI sight

---
+++ Assets/Scripts/ControlledObjects/AI/AISight.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AISight : MonoBehaviour {
+
+    public Eye[] Eyes;
+
+	
+    /// <summary>
+    /// Searches each eye for the player. If one of the eyes can see the player, it returns the 
+    /// player's transform. Otherwise, it returns null.
+    /// </summary>
+    public Transform LookForPlayer() {
+        Transform t = null;
+        foreach (Eye eye in Eyes) {
+            RaycastHit hit = eye.LookForPlayer();
+            if (hit.collider != null && hit.collider.gameObject == Player.Instance.gameObject) {
+                return hit.collider.transform;
+            }
+        }
+        return t;
+    }
+
+}
---
+++ Assets/Scripts/ControlledObjects/AI/Eye.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Eye : MonoBehaviour {
+
+    [Tooltip("The total field of view the eye can see within, in degrees.")]
+    public float FieldOfView = 120;
+    [Tooltip("The maximum distance the eye can detect the player within.")]
+    public float MaxSightDistance = Mathf.Infinity;
+
+    /// <summary>
+    /// Attempts to Raycast to the player's location. If the player is not within the field of view 
+    /// and within the max distance, the hit will have a null collider. If the there is an object in 
+    /// the way, the hit will return that object's collider. If the player is within the field of view
+    /// and max distance and there are no colliders in the way, it hit's collider will be the player.
+    /// </summary>
+    public RaycastHit LookForPlayer() {
+        Vector3 rayDirection = Player.Instance.getTransformPosition() - transform.position;
+        
+        if ((Vector3.Angle(rayDirection, transform.forward)) < FieldOfView / 2
+            && Vector3.Distance(transform.position, Player.Instance.getTransformPosition()) < MaxSightDistance) {
+                RaycastHit hit = new RaycastHit();
+                if (Physics.Raycast(transform.position, rayDirection, out hit, MaxSightDistance)) {
+                    return hit;
+                }
+        }
+        return new RaycastHit();
+    }
+}
