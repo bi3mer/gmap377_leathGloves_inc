eb21344e7fae4ca21c7ef0a02e21335aa63106d7
bi3mer <colan@comcast.net>
2015-11-04 18:17:52 -0500

    priority queue and astarnode implemented

--- Assets/Scripts/ControlledObjects/AI/AStar.cs
+++ Assets/Scripts/ControlledObjects/AI/AStar.cs
@@ -6,59 +6,56 @@ public class AStar : MonoBehaviour
 {
     public GameObject debugCube;
     public int distanceToGround = 100;
+    public int stepCount = 1;
+
+    // Target
+    public Vector3 target;
+
+    // Plan for movement
     private ArrayList plan;
 
-    public IEnumerator startPathFind()
-    {
-        yield return new WaitForSeconds(2.0f);
-        this.pathFinding();
-    }
-
-    public void pathFinding()
+    /// <summary>
+    ///     get path plan
+    /// </summary>
+    private ArrayList getThePath()
     {
         // http://docs.unity3d.com/ScriptReference/RaycastHit-triangleIndex.html
+        // Raycast downward
         RaycastHit[] hits;
         hits = Physics.RaycastAll(transform.position, transform.up * -1 * this.distanceToGround);
 
-        // Initialize
+        // Visited nodes
+        Dictionary<int, bool> visitedNodes = new Dictionary<int, bool>();
+
+        // Initialize open list
         ArrayList unExaminedMoves = new ArrayList();
 
+        // fill open list
         foreach (RaycastHit hit in hits)
         {
             MeshCollider meshCollider = hit.collider as MeshCollider;
-            if (meshCollider == null || meshCollider.sharedMesh == null)
-            {
-				print ("empty: " + hit.collider.tag);
-                continue;
-            }
-            else
+            if (meshCollider != null || meshCollider.sharedMesh != null)
             {
                 unExaminedMoves = VertexNavigation.Instance.getMovesTriangle(hit.triangleIndex * 3);
                 break;
             }
         }
 
-        foreach (int vertice in unExaminedMoves)
+        // Sort list
+        //foreach()
+        //{
+            
+        //}
+
+        return null;
+    }
+
+    public ArrayList getPath()
+    {
+        if (this.target == null)
         {
-            print("here!");
-            Instantiate(this.debugCube, VertexNavigation.Instance.getVertex(vertice).position, Quaternion.identity);
+            return null;
         }
-        Debug.Break();
-        
-        // Convert to priority queue via some sort
-        // var closest = mesh.vertices.OrderBy(v=>(v - pos).sqrMagnitude).Take(9).ToArray();
-
-        // A*, examine python for more details
-        // Need binary search here
-    }
-
-    void Start()
-    {
-        StartCoroutine(this.startPathFind());  
-    }
-
-    void Update()
-    {
-        Debug.DrawRay(transform.position, transform.up * -1 * this.distanceToGround, Color.green);
+        return this.getThePath();
     }
 }
---
+++ Assets/Scripts/ControlledObjects/AI/AStarNode.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AStarNode
+{
+    // Constructor
+    public AStarNode(int _g, int _h, int _index, ArrayList _moves)
+    {
+        this.G = _g;
+        this.H = _h;
+        this.Index = _index;
+        this.Moves = _moves;
+    }
+
+    // Step Count
+    private int g;
+    public int G
+    {
+        get
+        {
+            return this.g;
+        }
+        set
+        {
+            this.g = value;
+        }
+    }
+
+    // Heuristic val
+    private int h;
+    public int H
+    {
+        get
+        {
+            return this.h;
+        }
+        set
+        {
+            this.h = value;
+        }
+    }
+
+    // index
+    private int index;
+    public int Index
+    {
+        get
+        {
+            return this.index;
+        }
+        set
+        {
+            this.index = value;
+        }
+    }
+
+    // Moves
+    private ArrayList moves;
+    public ArrayList Moves
+    {
+        get
+        {
+            return this.moves;
+        }
+        set
+        {
+            this.moves = value;
+        }
+    }
+    /// <summary>
+    ///     return cost
+    /// </summary>
+    /// <returns></returns>
+    public int getCost()
+    {
+        return this.G + this.H;
+    }
+}
