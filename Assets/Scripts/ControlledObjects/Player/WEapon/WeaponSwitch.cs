a59d7bf88e574fd7f5533be8fbf9fec89c75c626
bfawber <brf35@drexel.edu>
2015-11-07 17:34:26 -0500

    Ammo/Weapon Switchin

---
+++ Assets/Scripts/ControlledObjects/Player/WEapon/WeaponSwitch.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeaponSwitch : MonoBehaviour {
+
+    PickupCache cache;
+
+    void Start()
+    {
+        cache = GetComponent<PickupCache>();
+    }
+
+    void Update()
+    {
+        if (InputManager.Player2HorizontalInput < 0 || Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            Debug.Log("Weapon Switch Default Laser");
+            GetComponent<Shooting>().bullet = cache.Laser;
+        }
+        if (InputManager.Player2HorizontalInput > 0 || Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            Debug.Log("Weapon Switch Rocket");
+            if (cache.Weapon2.GetComponent<Weapon>().Ammo > 0)
+            {
+                GetComponent<Shooting>().bullet = cache.Weapon2;
+            }
+        }
+        if (InputManager.Player2VerticalInput > 0 || Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            Debug.Log("Weapon Switch Laser Beam");
+            if (cache.LaserBeam.GetComponent<Weapon>().Ammo > 0)
+            {
+                GetComponent<Shooting>().bullet = cache.LaserBeam;
+            }
+        }
+        if (InputManager.Player2VerticalInput < 0 || Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            Debug.Log("Weapon Switch Mines");
+        }
+    }
+}
