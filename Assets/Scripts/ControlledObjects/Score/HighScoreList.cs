e11df6e5fd4d7b4686a9eac10e404498ae51e46e
rowlandsc <cr549@drexel.edu>
2015-11-12 02:36:07 -0500

    FINALLY We have high scores!
    -persistent high scores through out play throughs

--- Assets/Scripts/ControlledObjects/Score/HighScoreList.cs
+++ Assets/Scripts/ControlledObjects/Score/HighScoreList.cs
@@ -26,49 +26,107 @@ public class HighScoreList : MonoBehaviour {
 	// Use this for initialization
 	void Start () 
 	{
-		HighScoreListGUI = GetComponent<Text>();
+        //Use this to clear out the old high scores
+        //PlayerPrefs.DeleteAll();
+
+        LoadList();
+        HighScoreListGUI = GetComponent<Text>();
 		HighScoreListGUI.enabled = true;
 		
 		HighScoreTitle = HighScoreTitle.GetComponent<Text>();
 		HighScoreTitle.enabled = true;
-		
-		StoreHighScore(ScoreManager.Instance.score);
+
+        int newHighScore = ScoreManager.Instance.score;
+        string playerName = ScoreManager.Instance.GetName();
+        //StoreHighScore(ScoreManager.Instance.score);
+        StoreNewHighScore(newHighScore, playerName);
 		DisplayHighScore();
 	}
 	
 	
-	void StoreHighScore(int newHighScore)
-	{
+	//void StoreHighScore(int newHighScore)
+	//{
         
 
-		int oldHighScore = PlayerPrefs.GetInt("highScore", 0);    
+	//	int oldHighScore = PlayerPrefs.GetInt("highScore", 0);    
        
-		if(newHighScore > oldHighScore)
-		{
-			PlayerPrefs.SetInt("highScore", newHighScore);
-		}
+	//	if(newHighScore > oldHighScore)
+	//	{
+	//		PlayerPrefs.SetInt("highScore", newHighScore);
+	//	}
 		
-		PlayerPrefs.Save();
-	}
+	//	PlayerPrefs.Save();
+	//}
 
     void StoreNewHighScore(int newHighScore, string playerName)
     {
         PlayerScore a = new PlayerScore();
         a.name = playerName;
         a.score = newHighScore;
-
+        //LoadList();
         scoreIndex.Add(a);
         scoreIndex.Sort();
         scoreIndex.Reverse();
 
+
         PlayerPrefs.SetString("name0", scoreIndex[0].name);
         PlayerPrefs.SetInt("score0", scoreIndex[0].score);
+
         PlayerPrefs.SetString("name1", scoreIndex[1].name);
         PlayerPrefs.SetInt("score1", scoreIndex[1].score);
-    }
-	void DisplayHighScore()
-	{
+
+        PlayerPrefs.SetString("name2", scoreIndex[2].name);
+        PlayerPrefs.SetInt("score2", scoreIndex[2].score);
+
+        PlayerPrefs.SetString("name3", scoreIndex[3].name);
+        PlayerPrefs.SetInt("score3", scoreIndex[3].score);
+
+        PlayerPrefs.SetString("name4", scoreIndex[4].name);
+        PlayerPrefs.SetInt("score4", scoreIndex[4].score);
+
        
-		HighScoreListGUI.text = ((ScoreManager.Instance.GetName()) + " " +  PlayerPrefs.GetInt("highScore").ToString());
-	}
+        PlayerPrefs.Save();
+
+    }
+	
+    void LoadList()
+    {
+        PlayerScore a = new PlayerScore();
+        a.name = PlayerPrefs.GetString("name0");
+        a.score = PlayerPrefs.GetInt("score0");
+        scoreIndex.Add(a);
+
+        PlayerScore b = new PlayerScore();
+        b.name = PlayerPrefs.GetString("name1");
+        b.score = PlayerPrefs.GetInt("score1");
+        scoreIndex.Add(b);
+
+        PlayerScore c = new PlayerScore();
+        c.name = PlayerPrefs.GetString("name2");
+        c.score = PlayerPrefs.GetInt("score2");
+        scoreIndex.Add(c);
+
+        PlayerScore d = new PlayerScore();
+        d.name = PlayerPrefs.GetString("name3");
+        d.score = PlayerPrefs.GetInt("score3");
+        scoreIndex.Add(d);
+
+        PlayerScore e = new PlayerScore();
+        e.name = PlayerPrefs.GetString("name4");
+        e.score = PlayerPrefs.GetInt("score4");
+        scoreIndex.Add(e);
+
+
+       
+    }
+    void DisplayHighScore()
+	{
+        HighScoreListGUI.text =                          "1. " + scoreIndex[0].name + "    " + scoreIndex[0].score + "\n";
+        HighScoreListGUI.text = HighScoreListGUI.text  + "2. " + scoreIndex[1].name + "    " + scoreIndex[1].score + "\n";
+        HighScoreListGUI.text = HighScoreListGUI.text +  "3. " + scoreIndex[2].name + "    " + scoreIndex[2].score + "\n";
+        HighScoreListGUI.text = HighScoreListGUI.text +  "4. " + scoreIndex[3].name + "    " + scoreIndex[3].score + "\n";
+        HighScoreListGUI.text = HighScoreListGUI.text +  "5. " + scoreIndex[4].name + "    " + scoreIndex[4].score;
+
+        //HighScoreListGUI.text = ((ScoreManager.Instance.GetName()) + " " +  PlayerPrefs.GetInt("highScore").ToString());
+    }
 }
