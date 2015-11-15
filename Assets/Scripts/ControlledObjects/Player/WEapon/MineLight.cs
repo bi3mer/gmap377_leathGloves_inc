bab378af1d97c4e71987b522d2ed3aeb3bcc17f6
bfawber <brf35@drexel.edu>
2015-11-15 17:35:16 -0500

    Mine setting, sample laser particle, single laser shot allowed at one time, shorter laser, a little balancing

---
+++ Assets/Scripts/ControlledObjects/Player/WEapon/MineLight.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MineLight : MonoBehaviour
+{
+    public float LightOnIntensity, LightOffIntensity;
+    private Light light;
+    public float onTime, resetTime;
+    private bool on;
+
+    void Start()
+    {
+        this.light = GetComponent<Light>();
+    }
+
+    void Update()
+    {
+        if (onTime < float.Epsilon)
+        {
+            if (on)
+            {
+                this.light.intensity = this.LightOffIntensity;
+                this.on = false;
+            }
+            else
+            {
+                this.light.intensity = this.LightOnIntensity;
+                this.on = true;
+            }
+            this.onTime = resetTime;
+        }
+        else
+        {
+            this.onTime -= Time.deltaTime;
+        }
+    }
+   
+	
+}
