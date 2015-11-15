95d23ee3c9c26af6311783e3e72df33ebfeed2e4
bi3mer <colan@comcast.net>
2015-11-15 18:50:22 -0500

    Deleted Artillery plugin, and artillery behvaiour implemented

--- Assets/Scripts/ControlledObjects/AI/Weapon/EnemyArtillery.cs
+++ Assets/Scripts/ControlledObjects/AI/Weapon/EnemyArtillery.cs
@@ -3,17 +3,35 @@ using System.Collections;
 
 public class EnemyArtillery : MonoBehaviour 
 {
-	public float maxRange;
+	public float rotateSpeed = 20;
+	private float previousAngle = 0;
+	private float previousPreviousAngle = 0;
 
-	// Use this for initialization
-	void Start () 
+	void Start()
 	{
-	
+		this.previousAngle = Vector3.Angle(this.transform.forward, Player.Instance.transform.position - this.transform.position);
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-	
+		float angle = Vector3.Angle(this.transform.forward, Player.Instance.transform.position - this.transform.position);
+		print (previousAngle + " < " + angle);
+
+		// mathf.abs(angle) but just in one line :D
+		if(previousAngle < angle)
+		{
+			if(this.previousPreviousAngle > this.previousAngle)
+			{
+				print ("fire!!!");
+			}
+		} 
+		else
+		{
+			transform.Rotate(Vector3.up, Time.deltaTime * this.rotateSpeed);
+		}
+
+		this.previousPreviousAngle = this.previousAngle;
+		this.previousAngle = angle;
 	}
 }
