961914b615fb75080b92dcccd48720e4e20499ac
rowlandsc <cr549@drexel.edu>
2015-12-02 23:38:57 -0500

    LaserDestroy effect

--- Assets/Scripts/ControlledObjects/CollisionDestroy.cs
+++ Assets/Scripts/ControlledObjects/CollisionDestroy.cs
@@ -3,12 +3,15 @@ using System.Collections;
 
 public class CollisionDestroy : MonoBehaviour 
 {
-	/// <summary>
-	/// Raises the collision enter event destroys self on collison.
-	/// </summary>
-	/// <param name="collision">Collision.</param>
+    /// <summary>
+    /// Raises the collision enter event destroys self on collison.
+    /// </summary>
+    /// <param name="collision">Collision.</param>
+    public GameObject destroyEffect;
 	void OnCollisionEnter(Collision collision) 
 	{
-			Destroy(gameObject);
+        // Create Explosion object
+        Instantiate(destroyEffect, new Vector3(transform.position.x, transform.position.y, transform.position.z), transform.rotation);
+        Destroy(gameObject);
     }
 }
