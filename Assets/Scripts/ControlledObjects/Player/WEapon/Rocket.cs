b6de117c50de68f8dccb5395c5892e8efd5c8512
bi3mer <colan@comcast.net>
2015-12-05 15:15:57 -0500

    removed dumb print

--- Assets/Scripts/ControlledObjects/Player/WEapon/Rocket.cs
+++ Assets/Scripts/ControlledObjects/Player/WEapon/Rocket.cs
@@ -35,7 +35,6 @@ public class Rocket : MonoBehaviour
 
 				if(hitColliders[i].tag == "Player")
 				{
-					Debug.Log("herehewhrqwerhqwehr");
 					ScoreManager.Instance.DecreaseScore((int) this.GetComponent<Weapon>().damage);
 				}
 
