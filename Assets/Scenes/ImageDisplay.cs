0a90fc0f7b3d17804e95c5fdb4516c3feca0684b
rowlandsc <newadmin@n1-140-167.dhcp.drexel.edu>
2015-12-05 18:06:58 -0500

    Saturday
    +Splash Screen
    +Game Icon
    + Optimized Build Settings
    +Start Screen Text Fixed
    +New Start Screen Art

---
+++ Assets/Scenes/ImageDisplay.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ImageDisplay : MonoBehaviour 
+{
+
+	public Texture imageToDisplay;
+	public float timeToDisplayImage;
+	public int nextLevelToLoad;
+	
+	private float timeForNextLevel;
+	
+	public void Start() 
+	{
+		timeForNextLevel = Time.time + timeToDisplayImage;
+	}
+	
+	public void OnGUI() 
+	{
+		GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), imageToDisplay);
+		if (Time.time >= timeForNextLevel) 
+		{
+			Application.LoadLevel(nextLevelToLoad);
+		}
+	}
+}
