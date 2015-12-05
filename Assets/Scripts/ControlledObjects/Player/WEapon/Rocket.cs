4196972a6adb387e6a39a0b9b7bb54c8cc8b1a74
paperclip007 <alexbafaloukos@gmail.com>
2015-12-05 18:05:57 -0500

    Destructable environments

--- Assets/Scripts/ControlledObjects/Player/WEapon/Rocket.cs
+++ Assets/Scripts/ControlledObjects/Player/WEapon/Rocket.cs
@@ -20,30 +20,37 @@ public class Rocket : MonoBehaviour
     {
         // Get all the objects in a <ExplosionRadius> radius from where the bullet collided
         Collider[] hitColliders = Physics.OverlapSphere(transform.position, ExplosionRadius, TargetLayer);
-
+        
         // Create Explosion object
         Instantiate(Explosion, new Vector3(transform.position.x, transform.position.y, transform.position.z), transform.rotation);
         
         // For every object in the explosion
         for (int i = 0; i < hitColliders.Length; ++i )
         {
+            if (hitColliders[i]) {
                 // Add an explosion force of <ExplosionForce> to them
-                hitColliders[i].gameObject.GetComponentInParent<Rigidbody>().AddExplosionForce(ExplosionForce, transform.position, this.ExplosionRadius);
-              
+                Rigidbody rb = hitColliders[i].gameObject.GetComponent<Rigidbody>();
+                if (rb) {
+                    rb.AddExplosionForce(ExplosionForce, transform.position, this.ExplosionRadius);
+                }
+
                 // Try and find an EnemyHealth script on the gameobject hit.
                 EnemyStats enemyHealth = hitColliders[i].gameObject.GetComponentInParent<EnemyStats>();
 
-				if(hitColliders[i].tag == "Player")
-				{
-					ScoreManager.Instance.DecreaseScore((int) this.GetComponent<Weapon>().damage);
-				}
+                if (hitColliders[i].tag == "Player") {
+                    ScoreManager.Instance.DecreaseScore((int)this.GetComponent<Weapon>().damage);
+                }
+
+                if (hitColliders[i].tag == "DestructableEnvironment") {
+                    Destroy(hitColliders[i].gameObject);
+                }
 
                 // If the EnemyHealth component exist...
-                if (enemyHealth != null)
-                {
+                if (enemyHealth != null) {
                     // ... the enemy should take damage.
-                    enemyHealth.TakeDamage((int) this.GetComponent<Weapon>().damage);
+                    enemyHealth.TakeDamage((int)this.GetComponent<Weapon>().damage);
                 }
+            }
         }
 
         // Destroy the rocket
