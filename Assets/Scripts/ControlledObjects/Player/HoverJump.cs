526a2b2307b67aa69889d306760c15173d2202b2
rawhat <awm48@n1-142-102.dhcp.drexel.edu>
2016-01-09 14:52:26 -0500

    Added HoverJump
    Attached new HoverJump script to Player prefab
    “Jump” axis in InputManager allows player to jump
     - Height is “JumpForceMagnitude” and can be modified in the Inspector
     - Cooldown can also be modified in the Inspector

---
+++ Assets/Scripts/ControlledObjects/Player/HoverJump.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HoverJump : MonoBehaviour {
+	public float jumpDelay;
+	public float jumpForceMagnitude;
+
+	private Rigidbody playerRigidbody;
+	public  float jumpTimer;
+
+	void Start () {
+		playerRigidbody = GetComponent<Rigidbody> ();
+		jumpTimer = 0f;
+	}
+
+	void Update () {
+		// Check for timer cooldown and key input
+		if (Input.GetButtonDown("Jump") && jumpTimer <= 0f) 
+		{
+			// Gets `up` direction relative to player object
+			// and multiplies it by the modifiable magnitude
+			Vector3 jumpForce = transform.up * jumpForceMagnitude;
+			playerRigidbody.AddForce (jumpForce);
+
+			// Resets timer to public delay value
+			jumpTimer = jumpDelay;
+		} 
+		else
+		{
+			// Decrements the jump timer by time between Update calls
+			jumpTimer -= Time.deltaTime;
+		}
+	}
+}
