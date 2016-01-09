94c85815721976ec9c71aa3b6884c7c9cbf26deb
rawhat <awm48@n1-142-102.dhcp.drexel.edu>
2016-01-09 16:08:17 -0500

    Non-Functional Hover Tank Script
    not working yet, still in progress

---
+++ Assets/Scripts/ControlledObjects/Player/HoverTank.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HoverTank : MonoBehaviour {
+	public float maxHeight;
+	public float minHeight;
+	public float defaultHeight;
+	public float smoothingFactor;
+
+	private Rigidbody playerRigidbody;
+
+	void Start () {
+		playerRigidbody = GetComponent<Rigidbody> ();
+	}
+
+	void Update () {
+		RaycastHit hit;
+		if (Physics.Raycast (transform.position, -transform.up, out hit, LayerMask.NameToLayer("Environment")))
+		{
+			Debug.Log ("hit planet");
+			Vector3 heightVector = transform.position - hit.point;
+			Debug.DrawLine(Vector2.zero, heightVector, Color.red, 5f);
+			if(!(heightVector.magnitude >= maxHeight))
+			{
+				Vector3 desiredHeight = Vector3.ClampMagnitude(heightVector, defaultHeight);
+				playerRigidbody.MovePosition(Vector3.Lerp(transform.position, hit.point + desiredHeight, Time.deltaTime * smoothingFactor));
+			}
+		}
+	}
+}
