12906b0d0a20d2123e3c9d9ea346448e754a4a8a
bfawber <bfawber@gmail.com>
2016-01-10 17:40:27 -0500

    Laserbeam Revamp 1.0

---
+++ Assets/Scripts/ControlledObjects/Player/WEapon/LaserBeamCylinder.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaserBeamCylinder : MonoBehaviour {
+
+    public float GrowRate = 1.5f, OffsetSpeed = 2f;
+    public MeshRenderer mesh;
+    public string Target = "Enemy";
+
+	// Use this for initialization
+	void Start ()
+    {
+        mesh = GetComponent<MeshRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.localScale = new Vector3(1f, transform.localScale.y + GrowRate * Time.deltaTime, 1f);
+        mesh.material.mainTextureOffset = new Vector3(Time.time * OffsetSpeed, 0f, 0f);
+	}
+
+    void OnTriggerEnter(Collider col)
+    {
+        // If it's the right tag
+        if (col.transform.gameObject.tag == this.Target)
+        {
+            // Get the enemy info
+            EnemyStats enemyHealth = col.transform.gameObject.GetComponentInParent<EnemyStats>();
+            if (enemyHealth != null)
+            {
+                // ... the enemy should take damage.
+                enemyHealth.TakeDamage((int)this.GetComponentInParent<Weapon>().damage);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/ControlledObjects/Player/WEapon/LaserBeamCylinder2.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaserBeamCylinder2 : MonoBehaviour {
+
+    public float GrowthRate = 1.5f, MovementOffset = .1f;
+
+	// Use this for initialization
+	void Start () {
+       this.GrowthRate = GetComponent<LaserBeamCylinder>().GrowRate;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.localPosition = transform.localPosition + Vector3.forward * GrowthRate * MovementOffset * Time.deltaTime;
+        
+	}
+}
