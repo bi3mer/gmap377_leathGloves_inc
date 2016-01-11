926bd8bb83d0c523f066a79007b33f8234b0fcfa
bfawber <bfawber@gmail.com>
2016-01-11 11:44:43 -0500

    Commented the laserbeam revamp

--- Assets/Scripts/ControlledObjects/Player/WEapon/LaserBeamCylinder2.cs
+++ Assets/Scripts/ControlledObjects/Player/WEapon/LaserBeamCylinder2.cs
@@ -1,18 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class LaserBeamCylinder2 : MonoBehaviour {
-
+public class LaserBeamCylinder2 : MonoBehaviour
+{
+    /**
+    * Public Variable Description
+    * GrowthRate - How quickly the cylinder is scaling
+    * MovementOffset - A constant to multiply the GrowthRate by in order to make the movement
+    * and scaling work together in order to make it appear that the cylinder is growing in one direction
+    */
     public float GrowthRate = 1.5f, MovementOffset = .1f;
 
-	// Use this for initialization
-	void Start () {
+	/// <summary>
+    /// Used to initialize GrowthRate to the appropriate value
+    /// </summary>
+	void Start ()
+    {
        this.GrowthRate = GetComponent<LaserBeamCylinder>().GrowRate;
 	}
 	
-	// Update is called once per frame
-	void Update () {
+	/// <summary>
+    /// Called every frame. Moves the cylinder to create the growing effect.
+    /// </summary>
+	void Update ()
+    {
         transform.localPosition = transform.localPosition + Vector3.forward * GrowthRate * MovementOffset * Time.deltaTime;
-        
 	}
 }
--- Assets/Scripts/ControlledObjects/Player/WEapon/Laserbeam_Parentpicker.cs
+++ Assets/Scripts/ControlledObjects/Player/WEapon/Laserbeam_Parentpicker.cs
@@ -1,10 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Laserbeam_Parentpicker : MonoBehaviour {
+/// <summary>
+/// Used to parent the Laser Beam parent object to the Bullet spawn point in order to keep the 
+/// laser beam rotated with the player during motion
+/// </summary>
+public class Laserbeam_Parentpicker : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
+    /// <summary>
+    /// Used to set the parent of the transform to the Bullet Spawn
+    /// </summary>
+	void Start ()
+    {
         transform.parent = GameObject.Find("BulletSpawn").transform;
 	}
 }
