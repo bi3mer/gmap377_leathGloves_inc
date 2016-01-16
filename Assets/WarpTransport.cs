5df09a8fc7dafb9f35677bdebeb3e91cb90f94db
rawhat <alex41290@gmail.com>
2016-01-16 16:30:57 -0500

    Added Warp Pad Transport
    - Each warp pad will have a specific destination warp pad GameObject
    tied to its script
    - There is a public float for a delay to account for future animations
    - Currently basic functionality is to increase the position vector of
    the player's destination by the identity Vector3.up of the destination
    warp pad, so that the player spawns on top -- this should probably be
    changed to actually place it perfectly and programmatically
    -

---
+++ Assets/WarpTransport.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WarpTransport : MonoBehaviour {
+
+    public Transform destinationWarpPad;
+    public float transportDelaySeconds;
+
+    // REMOVE WHEN BOLTS ARE IN
+    public bool canTransport;
+
+    void OnTriggerStay(Collider collider)
+    {
+        // TODO:  add check for player bolt quantity
+        if (collider.gameObject.CompareTag("Player") && canTransport)
+        {
+            // Disable player movement while transport is happening
+            Player.Instance.GetComponent<ForceMover>().enabled = false;
+
+            StartCoroutine(TransportAfterTime());
+        }
+    }
+
+    /// <summary>
+    ///     Delays for transportDelaySeconds then moves the player
+    ///     and re-enables the ForceMover script
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator TransportAfterTime()
+    {
+        // Wait for the above specified time
+        yield return new WaitForSeconds(transportDelaySeconds);
+
+        // Sets player position to new warp pad location, plus an offset so they are above the warp pad
+        Player.Instance.transform.position = destinationWarpPad.position + destinationWarpPad.up;
+
+        // Sets player rotation to the same as the destination warp pad, which will be orientated properly
+        Player.Instance.transform.rotation = destinationWarpPad.rotation;
+
+        // Re-enables ForceMover script so player can move again
+        Player.Instance.GetComponent<ForceMover>().enabled = true;
+    }
+}
