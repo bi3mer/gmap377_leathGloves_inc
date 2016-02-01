a7ef07ad47fb4a3faacc05e37b1ca15078f215a5
rawhat <alex41290@gmail.com>
2016-01-31 22:19:18 -0500

    Modified Boss and Added Flying Enemy
    - Fixed boss movement to stop when the player is within its trigger
    range
    - Added enum in abstract class for vertex type
    - Made flying enemy prefab from model (probably won't be used)
    - Initial implementation of flying enemy movement script
    - Initial implementation of flying enemy bomb script

---
+++ Assets/Scripts/ControlledObjects/AI/Weapon/DropTheBomb.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DropTheBomb : MonoBehaviour {
+
+    public GameObject enemyBomb;
+    public Transform bombSpawnLocation;
+    public float bombVelocity;
+
+    public void dropBomb(Vector3 target)
+    {
+        GameObject bomb = Instantiate(enemyBomb, bombSpawnLocation.position, this.transform.rotation) as GameObject;
+        bomb.GetComponent<Rigidbody>().AddRelativeForce(bomb.transform.forward * bombVelocity, ForceMode.Impulse);
+        bomb.GetComponent<Rigidbody>().useGravity = true;
+    }
+}
