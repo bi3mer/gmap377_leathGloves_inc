6c4bfd422b054cd370490db1c1d929c7069142e8
bi3mer <colan@comcast.net>
2016-02-05 03:15:14 -0500

    Just a lot of changes
    added mini cut scene at the end

---
+++ Assets/DeleteThisScriptAferDemo.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeleteThisScriptAferDemo : MonoBehaviour 
+{
+    public Transform endMarker;
+    public Transform startMarker;
+    public float timeToMove = 10f;
+    public GameObject boss;
+
+    private Vector3 startPos;
+    private float startTime;
+    private float journeyLength;
+
+    void Start()
+    {
+        startMarker = this.transform;
+        startTime = Time.time;
+        journeyLength = Vector3.Distance(startMarker.position, endMarker.position);
+    }
+    void Update()
+    {
+        float distCovered = (Time.time - startTime) * timeToMove;
+        float fracJourney = distCovered / journeyLength;
+        transform.position = Vector3.Lerp(startMarker.position, endMarker.position, fracJourney);
+
+        transform.LookAt(this.boss.transform.position);
+    }
+}
+
+
--- Assets/WarpTransport.cs
+++ Assets/WarpTransport.cs
@@ -43,20 +43,34 @@ public class WarpTransport : MonoBehaviour {
     /// <returns></returns>
     IEnumerator TransportAfterTime()
     {
+        // TODO: remove this part below after demo ---->
+        GameObject cam = GameObject.FindGameObjectsWithTag("MainCamera")[0];
+        cam.transform.parent = null;
+        // -------------------------------------------->
+
         // Wait for the above specified time
         yield return new WaitForSeconds(transportDelaySeconds);
 
+        
+
         // Sets player position to new warp pad location, plus an offset so they are above the warp pad
-        Player.Instance.transform.position = destinationWarpPad.position + destinationWarpPad.up;
+        //Player.Instance.transform.position = destinationWarpPad.position + destinationWarpPad.up;
 
         // Sets player rotation to the same as the destination warp pad, which will be orientated properly
-        Player.Instance.transform.rotation = destinationWarpPad.rotation;
+        //Player.Instance.transform.rotation = destinationWarpPad.rotation;
 
         // Re-enables ForceMover script so player can move again
-        Player.Instance.GetComponent<ForceMover>().enabled = true;
+        // TODO: uncomment this COLANASDFJAS;LDKJA;SKLDFJALSKD;JAS;LKDJASL;KDJ;ALKSDFJLAK;SDF;LKASDFJAL;KSDFJ;LKJ
+        //Player.Instance.GetComponent<ForceMover>().enabled = true;
 
+        // TODO: remove this part below after demo ---->
+        yield return new WaitForSeconds(.5f);
+        cam.GetComponent<DeleteThisScriptAferDemo>().enabled = true;
+        // -------------------------------------------->
+
+        // TODO: Commented because this will break it
         // Re-enables SpawnSystem script to create new enemies
-        SpawnSystem.Instance.enabled = true;
+        //SpawnSystem.Instance.enabled = true;
     }
 
     public void ActivateTeleport()
