7145a5d979d782a4a2f5a68a2146e594393e71c1
rowlandsc <cr549@n1-140-51.dhcp.drexel.edu>
2016-02-06 17:31:19 -0500

    Falling rocks script and activator

---
+++ Assets/FallingRocksActivator.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+/// <summary>
+/// Falling rocks activator. Attach to a trigger and assign the objects you want to activate.
+/// </summary>
+public class FallingRocksActivator : MonoBehaviour 
+{
+	public GameObject[] FallingRocks;
+
+	/// <summary>
+	/// Checks for collison with the player and then activates rigidbodies on 
+	/// </summary>
+	/// <param name="activator">Activator.</param>
+	void OnTriggerEnter(Collider activator)
+	{
+		if (activator.tag == "Player") 
+		{
+			for (int i = 0; i < FallingRocks.Length; ++i) 
+			{
+				FallingRocks [i].GetComponent<Rigidbody> ().isKinematic = false;
+				FallingRocks [i].GetComponent<Rigidbody> ().detectCollisions = true;
+			}
+		}
+	}
+
+}
