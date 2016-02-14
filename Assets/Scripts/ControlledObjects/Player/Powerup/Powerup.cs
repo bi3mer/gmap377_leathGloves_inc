6eb49e7a80a035246fa5b2db42ab6938e92c2d1d
rowlandsc <cr549@drexel.edu>
2016-02-13 19:32:40 -0500

    Powerupbar v02 and icons

--- Assets/Scripts/ControlledObjects/Player/Powerup/Powerup.cs
+++ Assets/Scripts/ControlledObjects/Player/Powerup/Powerup.cs
@@ -4,13 +4,22 @@ public class Powerup
     public bool IsActive;
     public float Timer, SetTime;
     public GameObject GuiBar;
+	public GameObject guiIcon;
     
-    public Powerup(float setTime, GameObject guiBar)
+    public Powerup(float setTime, GameObject guiBar, GameObject guiIcon)
     {
         this.IsActive = false;
         this.Timer = 0f;
         this.SetTime = setTime;
         this.GuiBar = guiBar;
+		this.guiIcon = guiIcon;
         
     }
+	public Powerup(float setTime, GameObject guiBar)
+	{
+		this.IsActive = false;
+		this.Timer = 0f;
+		this.SetTime = setTime;
+		this.GuiBar = guiBar;		
+	}
 }
--- Assets/Scripts/GameManagers/PowerUpManager.cs
+++ Assets/Scripts/GameManagers/PowerUpManager.cs
@@ -6,12 +6,6 @@ public class PowerUpManager : MonoBehaviour
 {
 
     /**
-     * Constants Description
-     * TICK - The constant 1. Used to tick the timer down.
-     */
-    const float TICK = 1f;
-
-    /**
      * Public Variable Description
      * PowerUpManager - Singleton pattern. The instance of this object.
      * MultiShotTime - How long the multishot shall be active for
@@ -21,13 +15,14 @@ public class PowerUpManager : MonoBehaviour
      * PowerIncrease - How much extra damage the damage up power up will grant
      */
     public GameObject ShieldModel;
-    public GameObject MultiBar, DamageUpBar, ShieldBar, SpeedBar;
-    public float MultiShotTime = 10f, DamageUpTime = 10f, ShieldTime = 10f, SpeedBoostTime = 10f, MultiOffset = 300f, MultiShotAngle = 45f, PowerIncrease = 10f,
+	public GameObject MultiBar, DamageUpBar, ShieldBar, SpeedBar, MultiBarIcon, DamageUpBarIcon, ShieldBarIcon, SpeedBarIcon;
+	public float MultiShotTime = 10f, DamageUpTime = 10f, ShieldTime = 10f, SpeedBoostTime = 10f, MultiOffset = 300f, MultiShotAngle = 45f, PowerIncrease = 10f,
         BombRadius = 15f, BombDamage = 200f, SpeedBoostAmount = 1.5f, CurrentSpeedBoost = 1f, ShieldResizer = -23f;
     public LayerMask BombLayer;
     public float CurrentLaserCount =  0f;
     public Powerup Multishot, DamageUp, Shield, SpeedBoost, Bomb;
     public Dictionary<string, Powerup> Powerups;
+
 
     /**
      * Private Variable Description
@@ -46,10 +41,10 @@ public class PowerUpManager : MonoBehaviour
         {
             Instance = this;
             this.Powerups = new Dictionary<string, Powerup>();
-            this.Multishot = new Powerup(MultiShotTime, MultiBar);
-            this.DamageUp = new Powerup(DamageUpTime, DamageUpBar);
-            this.Shield = new Powerup(ShieldTime, ShieldBar);
-            this.SpeedBoost = new Powerup(SpeedBoostTime, SpeedBar);
+			this.Multishot = new Powerup(MultiShotTime, MultiBar, this.MultiBarIcon);
+			this.DamageUp = new Powerup(DamageUpTime, DamageUpBar, this.DamageUpBarIcon);
+			this.Shield = new Powerup(ShieldTime, ShieldBar, this.ShieldBarIcon);
+			this.SpeedBoost = new Powerup(SpeedBoostTime, SpeedBar, this.SpeedBarIcon);
             this.Bomb = new Powerup(0f, null);
 
             Powerups.Add("Multishot", this.Multishot);
@@ -57,6 +52,8 @@ public class PowerUpManager : MonoBehaviour
             Powerups.Add("Shield", this.Shield);
             Powerups.Add("SpeedBoost", this.SpeedBoost);
             Powerups.Add("Bomb", this.Bomb);
+
+
         }
         else
         {
@@ -82,6 +79,8 @@ public class PowerUpManager : MonoBehaviour
             Powerups[Key].Timer += Powerups[Key].SetTime;
             Powerups[Key].GuiBar.GetComponent<PowerUpBar>().activeTime = Powerups[Key].SetTime;
             Powerups[Key].GuiBar.GetComponent<PowerUpBar>().activate();
+			Powerups[Key].GuiBar.GetComponent<PowerUpBar>().transform.SetAsFirstSibling();
+			Powerups[Key].guiIcon.SetActive(true);
 
             if (Key.Equals("Multishot"))
             {
@@ -100,6 +99,9 @@ public class PowerUpManager : MonoBehaviour
             Powerups[Key].Timer += Powerups[Key].SetTime;
             Powerups[Key].GuiBar.GetComponent<PowerUpBar>().activeTime = Powerups[Key].Timer;
             Powerups[Key].GuiBar.GetComponent<PowerUpBar>().activate();
+			Powerups[Key].GuiBar.GetComponent<PowerUpBar>().transform.SetAsFirstSibling();
+
+			Powerups[Key].guiIcon.SetActive(true);
         }
     }
 
@@ -107,12 +109,13 @@ public class PowerUpManager : MonoBehaviour
     {
         while(Powerups[Key].Timer > 0f)
         {
-            Debug.Log("Time Left: " + Powerups[Key].Timer);
+    
             this.Powerups[Key].Timer -= Time.deltaTime;
             yield return null;
         }
 
         this.Powerups[Key].IsActive = false;
+		Powerups[Key].guiIcon.SetActive(false);
 
         if (Key.Equals("Multishot"))
         {
@@ -128,32 +131,5 @@ public class PowerUpManager : MonoBehaviour
         {
             this.CurrentSpeedBoost = 1f;
         }
-
-        //if (this.Powerups[Key].Timer > 0f)
-        //{
-        //    Debug.Log("Time Left: " + Powerups[Key].Timer);
-        //    --this.Powerups[Key].Timer;
-        //    yield return new WaitForSeconds(TICK);
-        //    StartCoroutine(Timer(Key));
-        //}
-        //else
-        //{
-        //    this.Powerups[Key].IsActive = false;
-
-        //    if (Key.Equals("Multishot"))
-        //    {
-        //        this.maxLaserCount = 1f;
-        //    }
-
-        //    if (Key.Equals("Shield"))
-        //    {
-        //        Destroy(GameObject.Find("Shield(Clone)"));
-        //    }
-
-        //    if(Key.Equals("SpeedBoost"))
-        //    {
-        //        this.CurrentSpeedBoost = 1f;
-        //    }
-        //}
     }
 }
