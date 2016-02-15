15f025e09867ece6048f141548c0cf5d65b3ff4e
rawhat <alex41290@gmail.com>
2016-02-14 22:04:39 -0500

    Movement Switcher Script
    - Attach both MoveTowardsPlayer and MoveAwayFromPlayer to an enemy,
    along with MoveDirectionSelector.
    - It takes a float for distance threshold to switch

---
+++ Assets/Scripts/ControlledObjects/AI/Behaviour/MoveDirectionSelector.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveDirectionSelector : MonoBehaviour {
+
+    public float distanceThreshold;
+
+    private MoveTowardsPlayer towardsPlayer;
+    private MoveAwayFromPlayer awayFromPlayer;
+
+    void Awake()
+    {
+        towardsPlayer = this.GetComponent<MoveTowardsPlayer>();
+        awayFromPlayer = this.GetComponent<MoveAwayFromPlayer>();
+        StartCoroutine(TowardsPlayer());
+    }
+
+    void Update() { }
+
+    IEnumerator TowardsPlayer()
+    {
+        while(DistanceCalculator.squareEuclidianDistance(Player.Instance.transform.position, this.transform.position) >= distanceThreshold)
+        {
+            yield return new WaitForSeconds(1f);
+        }
+        towardsPlayer.enabled = false;
+        awayFromPlayer.enabled = true;
+        StartCoroutine(AwayFromPlayer());
+    }
+
+    IEnumerator AwayFromPlayer()
+    {
+        while(DistanceCalculator.squareEuclidianDistance(Player.Instance.transform.position, this.transform.position) <= distanceThreshold)
+        {
+            yield return new WaitForSeconds(1f);
+        }
+        awayFromPlayer.enabled = false;
+        towardsPlayer.enabled = true;
+        StartCoroutine(TowardsPlayer());
+    }
+}
---
+++ Assets/Scripts/ControlledObjects/AI/Utility/BossDelay.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BossDelay : MonoBehaviour {
+
+    private MoveTowardsPlayer towardsPlayer;
+    private MoveAwayFromPlayer awayFromPlayer;
+    private MoveDirectionSelector selector;
+	private float delayInSeconds = 5f;
+
+	void Start () {
+        selector = this.GetComponent<MoveDirectionSelector>();
+        towardsPlayer = this.GetComponent<MoveTowardsPlayer>();
+        awayFromPlayer = this.GetComponent<MoveAwayFromPlayer>();
+
+        selector.enabled = false;
+        towardsPlayer.enabled = false;
+        awayFromPlayer.enabled = false;
+
+		StartCoroutine (CountdownTimer ());
+	}
+
+	IEnumerator CountdownTimer(){
+		yield return new WaitForSeconds (delayInSeconds);
+        selector.enabled = true;
+        towardsPlayer.enabled = true;
+	}
+}
