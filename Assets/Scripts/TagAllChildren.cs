af5f0ba66d1a6575d4a18e42233e156f73f1cc9b
bi3mer <colan@comcast.net>
2016-02-27 13:50:03 -0500

    Tagged All Enemy Prefabs

---
+++ Assets/Scripts/TagAllChildren.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class TagAllChildren : MonoBehaviour 
+{
+	public string Tag;
+
+	/// <summary>
+	/// Recursively tag all children
+	/// </summary>
+	/// <param name="transform">Transform.</param>
+	private void recursiveTag(Transform tran)
+	{
+		// Change tag
+		tran.tag = this.Tag;
+		
+		// Loop through children
+		for(int i = 0; i < tran.childCount; ++i)
+		{
+			this.recursiveTag(tran.GetChild(i));
+		}
+	}
+		
+	/// <summary>
+	/// Tags the children with public string Tag
+	/// </summary>
+	public void TagChildren()
+	{
+		this.recursiveTag(this.transform);
+	}
+}
+
+// Create menu declaration in editor
+#if UNITY_EDITOR
+[CustomEditor(typeof(TagAllChildren))]
+public class TagAllChildrenMenu : Editor 
+{
+	public override void OnInspectorGUI()
+	{
+		DrawDefaultInspector();
+		if(GUILayout.Button("Tag All Children"))
+		{
+			foreach (Object targ in targets) 
+			{
+				TagAllChildren tagging = targ as TagAllChildren;
+				tagging.TagChildren();
+			}
+			
+			Debug.Log("Tagged!");
+		}
+	}
+	
+	void OnInspectorUpdate()
+	{
+		Repaint();
+	}
+}
+#endif
