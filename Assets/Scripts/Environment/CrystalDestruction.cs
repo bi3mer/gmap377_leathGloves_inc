c8c723a558c8d0f715dfc2c1471c1212b5055206
bfawber <bfawber@gmail.com>
2016-02-27 18:04:06 -0500

    Started on crytstal destruction, and finished default laser auto fire

---
+++ Assets/Scripts/Environment/CrystalDestruction.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CrystalDestruction : MonoBehaviour {
+
+    public GameObject DestructionEffect;
+
+    void OnTriggerEnter(Collider col)
+    {
+        Instantiate(this.DestructionEffect, transform.position,transform.parent.rotation);
+       
+        Destroy(this.gameObject);
+    }
+}
